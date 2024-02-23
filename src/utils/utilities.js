fe6c91d6c5bf1063ab1c64858232a7400c10d0d7
trufflepirate <rootfourtytwo@gmail.com>
2024-02-23 21:12:52 +0800

    shippable3

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,11 +1,11 @@
 function get_base_WPA() {
   const urlparam = new URLSearchParams(window.location.search);
   const template = { SSID: '', domain: '', username: '', password: '', backupSSID: '', backupPassword: '' , primaryEnabled: true, backupEnabled: true};
-  template.SSID = urlparam.get('SSID');
+  template.SSID = urlparam.get('ssid');
   template.domain = urlparam.get('domain');
   template.username = urlparam.get('username');
   template.password = urlparam.get('password');
-  template.backupSSID = urlparam.get('backupSSID');
+  template.backupSSID = urlparam.get('backupssid');
   template.backupPassword = urlparam.get('backupPassword');
   return template;
 }
